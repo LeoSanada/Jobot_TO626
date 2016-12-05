b08484571eb1e4f77eebcbfe994cb03de34693ec
LeoSanada <rsanada@umich.edu>
2016-12-05 10:08:33 -0500

    add

---
+++ app/src/main/java/com/example/ryo/jobot_to626/CallActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryo.jobot_to626;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CallActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}
