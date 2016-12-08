42ce5cd5dd7ae5f5685b55d8e642ab45fd02d642
Crystal <crystalwang0124@gmail.com>
2016-12-08 13:25:00 -0500

    Jing version0.01 12/8/2016 1:24pm

--- app/src/main/java/com/example/ryo/jobot_to626/LoginActivity.java
+++ app/src/main/java/com/example/ryo/jobot_to626/LoginActivity.java
@@ -2,8 +2,19 @@ package com.example.ryo.jobot_to626;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class LoginActivity extends Activity {
+    private Button buttonLogin;
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPWD;
+    private RadioButton radioButtonStudent;
+    private RadioButton radioButtonProfessional;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
