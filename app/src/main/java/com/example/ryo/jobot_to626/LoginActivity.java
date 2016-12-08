6121b8416040dfd0c3da443899534f93cff140c8
Crystal <crystalwang0124@gmail.com>
2016-12-08 14:42:34 -0500

    Jing version0.011 12/8/2016 2:42pm

--- app/src/main/java/com/example/ryo/jobot_to626/LoginActivity.java
+++ app/src/main/java/com/example/ryo/jobot_to626/LoginActivity.java
@@ -2,11 +2,12 @@ package com.example.ryo.jobot_to626;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener{
     private Button buttonLogin;
     private Button buttonRegister;
     private EditText editTextEmail;
@@ -14,11 +15,30 @@ public class LoginActivity extends Activity {
     private RadioButton radioButtonStudent;
     private RadioButton radioButtonProfessional;
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPWD = (EditText) findViewById(R.id.editTextPWD);
+        radioButtonStudent =(RadioButton) findViewById(R.id.radioButtonStudent);
+        radioButtonProfessional = (RadioButton) findViewById(R.id.radioButtonProfessional);
+
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+        radioButtonProfessional.setOnClickListener(this);
+        radioButtonStudent.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
