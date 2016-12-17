ca99355df1044ae5d8fad6c7d1fccd8a16f06289
abazbaz <abazbaz@umich.edu>
2016-12-17 12:59:27 -0500

    Bio Activity added

--- app/src/main/java/com/example/ryo/jobot_to626/BioActivity.java
+++ app/src/main/java/com/example/ryo/jobot_to626/BioActivity.java
@@ -1,13 +1,35 @@
 package com.example.ryo.jobot_to626;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.Button;
 
 public class BioActivity extends Activity {
 
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bio);
+
+        //BUTTON TO SEE PROFILE
+        back = (Button) findViewById(R.id.backbutton);
+
+        back.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent newPage = new Intent(BioActivity.this, CallActivity.class);
+                startActivity(newPage);
+
+            }
+        });
+
+
     }
 }
